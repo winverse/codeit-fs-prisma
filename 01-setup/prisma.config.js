26d2f03fff3981423861cd8646fdfbc26b79796c
winverse <public.winverse@gmail.com>
2025-12-30 17:14:01 +0900

    feat(setup): Introduce .env.example and configure dotenv for Prisma

--- 01-setup/prisma.config.js
+++ 01-setup/prisma.config.js
@@ -1,4 +1,10 @@
+import dotenv from 'dotenv';
 import { defineConfig, env } from 'prisma/config';
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const envPath = `./env/.env.${nodeEnv}`;
+
+dotenv.config({ path: envPath });
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
