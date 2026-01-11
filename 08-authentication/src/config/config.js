910af6abffdcef2963690b849b8cf8bd887e69ea
winverse <public.winverse@gmail.com>
2026-01-11 12:23:09 +0900

    fix: Correctly load JWT_REFRESH_SECRET from environment variables

--- 08-authentication/src/config/config.js
+++ 08-authentication/src/config/config.js
@@ -17,7 +17,7 @@ const parseEnvironment = () => {
       PORT: process.env.PORT,
       DATABASE_URL: process.env.DATABASE_URL,
       JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
-      JWT_REFRESH_SECRET: config.JWT_REFRESH_SECRET,
+      JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
     });
   } catch (error) {
     if (error instanceof z.ZodError) {
